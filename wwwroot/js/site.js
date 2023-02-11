c77fca1479cf572f4b73fa89ea7c80085e8ec8e1
I74L0 <i7alog4m3r1@gmail.com>
2023-02-11 12:28:04 -0300

    Temporary changes

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,15 @@
-﻿document.querySelector("#selector").addEventListener("click", function () {
-    this.classList.toggle('off');
+﻿document.querySelector(".button").addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle('on');
 });
+
+let inputs = document.querySelectorAll(".light");
+inputs.forEach(element => {
+    element.addEventListener("click", function () {
+        this.querySelector("input").click();
+        this.querySelector("input").addEventListener("change", function () {
+            element.style.color = this.value;
+            element.style.backgroundColor = this.value;
+        });
+    });
+});
+
